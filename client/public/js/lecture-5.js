f6516610fd962649282cc6533b23c5aeb4abd3cd
rohit_goyal_ola <07goelrohit@gmail.com>
2020-08-24 11:34:17 +0530

    lecture-5

--- client/public/js/lecture-5.js
+++ client/public/js/lecture-5.js
@@ -2,12 +2,15 @@
  * TYPE COERCION
  */
 
-// 1 == "1";
-// 1 == 1
+/**
+ * USEFUL LINKS
+ * https://www.freecodecamp.org/news/js-type-coercion-explained-27ba3d9a2839/
+ * https://www.w3schools.com/js/js_type_conversion.asp
+ */
 
-// 6 > true; // 6 > 1
-// 6 > 1;
-// false > 1; // 0 > 1
+1 == "1"; // true
+
+6 > true; // 6 > 1
 
 // falsy consitions
 // null
@@ -17,33 +20,31 @@
 // NaN
 // ""
 
-//false -> 0
+// comparing boolean to a number
+// false -> 0
 // true -> 1
 
 // 1 + "2"; // number gets converted to string
 // "1" + "2";
 // ("12");
 
-// 1 + 2 + "3";
-// 3 + "3";
-// "3" + "3";
-// ("33");
+//1 + 2 + "3"; // 3 + "3" // "33"
 
 // 1 - "2";
 // 1 - 2;
 // -1;
 
-// 1 + "rohit";
+// 1 + "js";
 
 // --> Explicit conversion to String
-// 5 + ""; // strings
-// [1] + ""; // toString()
+// 5 + ""; // "5"
+// [1] + ""; //  array to a string ----- toString()
 // [1, 2] + [3, 4]; // toString()
-// [1,2] + 3
+// [1, 2] + 3;
 
 // --> Explicit conversion to Number
 // 4 > "3";
-// 4 / "2"; //
+// 4 / "2";
 
 // --> Explicit conversion to Boolean
 // if ({}) {
@@ -56,46 +57,41 @@
 /**
  * PRACTICE QUESTIONS
  */
-// 1 > 2 > 3; //  1 > 2 > 3 ---> false > 3 --> 0>3 // false
-// 6 < 7 < 8; // true < 8 --> 1 <8 ---> true
-// 1 + 2 + "3"; // 3 + "3"  ----> "33"
-// "1" + 2 + 3; // "12" +  3 ---> "123"
-// null > undefined; // 0 > 0 --> false
+// 1 > 2 > 3;  // false > 3
+// 6 < 7 < 8;  // true < 8
+// 1 + 2 + "3";
+// "1" + 2 + 3;
+// null > undefined;
 
-// === -->  == + it does a extra type check also
+// === -->  == (+ it does an extra type check also)
 
+// type coercion works only when there is "=="
 /**
  * PRACTICE QUESTION - 1
  */
-// let a = 1;
-// let b = "1";
-// console.log(a == b, a === b);
+let a = 1;
+let b = "1";
+console.log(a == b, a === b);
 
 /**
  * PRACTICE QUESTION - 2
 //  */
-// let a = 1.0;
-// let b = 1;
-// console.log(a == b, a === b);
+let a = 1.0;
+let b = 1;
+console.log(a == b, a === b);
 
 /**
  * PRACTICE QUESTION - 3
  */
-// let a = null;
-// let b = undefined;
-// console.log(a == b, a === b);
+let a = null;
+let b = undefined;
+console.log(a == b, a === b);
 
 // null == undefined;
 
-//===
+// ===  --- checks for the same data type first and then the same value
 // 1. check if the data types are same, if not return false
-// 2. if the types are same on both the extends, check for == or the value
-
-/**
- * USEFUL LINKS
- * https://www.freecodecamp.org/news/js-type-coercion-explained-27ba3d9a2839/
- * https://www.w3schools.com/js/js_type_conversion.asp
- */
+// 2. if the types are same on both the extremes, check for == or the value
 
 /**
  * OBJECTS BASICS
